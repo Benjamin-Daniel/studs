048cba7008079ad00357e39efbd53add7ecc6a08
Benjamin-Daniel <benjamindaniel1999.com>
2017-11-04 23:19:36 +0100

    removed router.js

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ var _mongoose = require('mongoose');
 
 var _mongoose2 = _interopRequireDefault(_mongoose);
 
-var _routes = require('./app/routes');
-
-var _routes2 = _interopRequireDefault(_routes);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var path = require('path');
@@ -53,13 +49,185 @@ app.use((0, _morgan2.default)('dev'));
 app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: false }));
 
+var Student = require('./models/Student');
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + './main/build/index.html');
+});    
+
+
+function checkforduplicate(newStudent, database) {
+    var check = false;
+    var one = newStudent;
+    database.map(function (two) {
+        if (one.name === two.name && one.otherNames === two.otherNames && one.level === two.level) {
+            check = true
+        }
+    })
+    return check
+}
+function checkforduplicateEmail(newStudent, database) {
+    var check = false;
+    var one = newStudent;
+    database.map(function (two) {
+        if (one.email === two.email) {
+            check = true
+        };
+
+
+    })
+    return check
+}
+function checkNumberOfEmail(newStudent, database) {
+    var check = 0;
+    var one = newStudent;
+    database.map(function (two) {
+        if (one.email === two.email) {
+            check++;
+        }
+    })
+    return check
+}
+
+
+
+app.get('/api', function (req, res) {
+    Student.find({}, function (err, foundData) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send();
+        }
+        return res.status(200).send(foundData);
+    })
 });
 
+app.post('/api/add', function (req, res) {
+    var name = req.body.name;
+    var otherNames = req.body.otherNames;
+    var email = req.body.email;
+    var level = req.body.level || 'beginner';
+    var sex = req.body.sex || 'It';
+    level.toLowerCase();
+    sex.toLowerCase();
 
-//set the routes
-app.use(_routes2.default);
+
+    //find all the student in the database
+    Student.find({}, function (err, foundData) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send({ error: 'there was an error internally daniel id fixing that' });
+        } else {
+            var newStudent = new Student();
+            if (name) {
+                newStudent.name = name;
+            }
+            if (email) {
+                newStudent.email = email;
+            }
+            if (level) {
+                newStudent.level = level;
+            }
+            if (otherNames) {
+                newStudent.otherNames = otherNames;
+            }
+            if (sex) {
+                newStudent.sex = sex;
+            }
+            newStudent.added = new Date().toDateString();
+            var check = checkforduplicate(newStudent, foundData);
+            var checkEmail = checkforduplicateEmail(newStudent, foundData);
+            if (checkEmail === true) {
+                return res.status(500).send({ error: "A fellow student has this email" })
+            } else {
+                if (check === true) {
+                    return res.send({ error: 'a student like that already exist' });
+                } else {
+
+                    newStudent.save(function (err, savedObject) {
+                        if (err) {
+                            console.log(err);
+                            return res.status(500).send()
+                        }
+                        //am redirecting it so that i can see the complete database
+                        //because the found data is just the data before the query
+                        return res.redirect('/api');
+                    });
+                }
+            }
+        }
+    })
+})
+
+app.put('/api/:id', function (req, res) {
+    var _id = req.params.id;
+    var name = req.body.name;
+    var email = req.body.email;
+    var otherNames = req.body.otherNames;
+    var level = req.body.level || 'beginner';
+    var sex = req.body.sex || 'It';
+    level.toLowerCase();
+    sex.toLowerCase();
+
+    //find all the students
+    Student.find({}, function (err, all) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send({ error: 'sorry some thing went wrong internally 👻' });
+        } else {
+            //find the student that neeeds to be updated
+            //check if other people have that email apart for the student
+            Student.findById({ _id }, function (err, student) {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).send({ error: 'sorry some thing went wrong internally 👻' })
+                }
+                //if the student isn't found 
+                if (!student) {
+                    res.status(404).send({ error: 'the student with this id wasn\'t found' })
+                }
+                else {
+                    //if the student was found
+                    student.email = email;
+                    //this checks the number of people with this email
+                    //it will give one 
+                    var checkEmail = checkNumberOfEmail(student, all);
+                    if (checkEmail >= 2) {
+                        return res.status(500).send({ error: "There is a student with this email." })
+                    } else {
+                        student.name = name;
+                        student.sex = sex;
+                        student.otherNames = otherNames;
+                        student.level = level;
+                        student.save(function (err, savedObject) {
+                            if (err) {
+                                console.log(err);
+                                return res.status(500).send()
+                            }
+                            return res.status(200).send(savedObject);
+                        });
+                    }
+                }
+            });
+        }
+    });
+});
+
+app.delete('/api/:id', function (req, res) {
+    var _id = req.params.id;
+    Student.findByIdAndRemove({ _id }, function (err, deletedObject) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send({ error: 'sorry some thing went wrong internally 👻' })
+        } else {
+            if (!deletedObject) {
+                return res.status(404).send({ error: 'the student with this id wasn\'t found' })
+            } else {
+                res.status(200).send({ sucess: `the student with the id ${_id} has been successfully deleted ` })
+            }
+        };
+    });
+});
+
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname + '/main/build/index.html'));
